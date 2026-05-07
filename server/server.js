9dae91405e95d0c1bbaf5bd8ebf386db356a6e85
Gary Singh <badjatt@MacBook-Pro-22.local>
2026-05-07 17:13:15 -0500

    fix render port

--- server/server.js
+++ server/server.js
@@ -29,8 +29,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+   app.listen(process.env.PORT || 5001, () => {
+  console.log(
+    `Server running on port ${process.env.PORT || 5001}`
+  );
+});
   })
   .catch((err) => console.log(err));
